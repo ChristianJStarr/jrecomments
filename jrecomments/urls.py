39e37d22e34b20f4693ec5ace8543a237e032c42
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-11-04 04:12:46 -0400

    Update urls.py

--- jrecomments/urls.py
+++ jrecomments/urls.py
@@ -25,8 +25,7 @@ urlpatterns = [
     path('action/comment/<id>/<parent_id>/<reply_to_id>/', views.comment, name='comment'),
     path('action/like/<id>/', views.like_comment, name='like'),
     path('action/dislike/<id>/', views.dislike_comment, name='dislike'),
-    path('action/nickname/set/<nickname>/', views.set_nick, name='setnick'),
-    path('iwontbuydiscordnitro/andyeah/', views.dan),
+    path('action/nickname/set/<nickname>/', views.set_nick, name='setnick')
 ]
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
