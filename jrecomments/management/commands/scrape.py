30b872fb342cc2ad86d931cef672f8ea0184cbbf
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-11-22 17:09:49 -0500

    Update scrape.py

--- jrecomments/management/commands/scrape.py
+++ jrecomments/management/commands/scrape.py
@@ -1,8 +1,10 @@
 from django.core.management import BaseCommand
 
+from jrecomments.models import Comment
 from jrecomments.views import calculate_comment_popularity, calculate_podcast_popularity
 from jrecomments.youtube import youtube_pull_comments
 
 
 class Command(BaseCommand):
+    Comment.objects.all().delete()
     youtube_pull_comments(50000)
